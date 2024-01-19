14776344940e3cfaabdf4e2f5d1579b3e3b268e1
wpanganiban <wendy@lunik.tech>
2024-01-19 15:27:00 +0000

    removed uncessary files

--- common_provision.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update -y
-ufw disable
-sleep 5
-
-#All access if fw is enabled
-# ufw allow 6443/tcp #apiserver
-# ufw allow from 10.42.0.0/16 to any #pods
-# ufw allow from 10.43.0.0/16 to any #services
